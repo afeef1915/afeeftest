6fdb9a4cb5ec81040cd7bca76178828924c0d403
afeef1915 <afeef1915@gmail.com>
2017-06-25 22:30:08 +0530

    data saved all ok

--- src/Acme/Bundle/BitcoinBundle/Controller/DefaultController.php
+++ src/Acme/Bundle/BitcoinBundle/Controller/DefaultController.php
@@ -3,6 +3,13 @@
 namespace Acme\Bundle\BitcoinBundle\Controller;
 header('Access-Control-Allow-Origin: *'); 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\HttpFoundation\Cookie;
+//use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -19,15 +26,32 @@ class DefaultController extends Controller
         return $this->render('AcmeBitcoinBundle:Graphs:graphs.html.twig');
     }
 
+     public function AlertAction()
+    {
+        return $this->render('AcmeBitcoinBundle:Bitcoin:alert.html.twig');
+    }
+     public function MangeAlertAction()
+    {
+        return $this->render('AcmeBitcoinBundle:Bitcoin:manage-alert.html.twig');
+    }
+    
     public function  ApiDataAction()
     {
-            $curl = curl_init();
-            curl_setopt($curl, CURLOPT_URL,"http://www.bitcoinrates.in/getdata.php"); 
-            curl_setopt ($curl, CURLOPT_POST, true);
-            curl_setopt ($curl, CURLOPT_POSTFIELDS, $data);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-            $result = curl_exec($curl);
-            curl_close($curl);
-            echo $result;exit();
-    }
+            // $curl = curl_init();
+            // curl_setopt($curl, CURLOPT_URL,"http://www.bitcoinrates.in/getdata.php"); 
+            // curl_setopt ($curl, CURLOPT_POST, true);
+            // curl_setopt ($curl, CURLOPT_POSTFIELDS, $data);
+            // curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            // $result = curl_exec($curl);
+            // curl_close($curl);
+            // echo $result;exit();
+            $url="http://www.bitcoinrates.in/getdata.php";
+            $result = file_get_contents($url);
+            $data=json_decode($result, true);
+            $json= json_encode($data);
+
+            print_r($json);
+            die;
+        }
+            
 }
